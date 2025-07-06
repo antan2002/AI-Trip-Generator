28f44df826c31af51c5163f72aef242468b51844
antan2002 <antanroy502@gmail.com>
2025-07-06 21:23:47 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-require('./Model/db')
+require('./Model/db');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const AuthRouter = require('./Routes/AuthRoutes')
-const TripRouter = require('./Routes/TripRoutes')
+const AuthRouter = require('./Routes/AuthRoutes');
+const TripRouter = require('./Routes/TripRoutes');
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping', (req, res) => {
-    res.send('PONG');
-})
+const allowedOrigins = [
+    'https://darling-crepe-6e2303.netlify.app',
+    'http://localhost:3000'
+];
 
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(cors());
+
 app.use('/trip', AuthRouter);
 app.use('/trip', TripRouter);
 
+app.get('/ping', (req, res) => {
+    res.send('PONG');
+});
+
 app.listen(PORT, () => {
-    console.log(`server is running , ${PORT} `)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
